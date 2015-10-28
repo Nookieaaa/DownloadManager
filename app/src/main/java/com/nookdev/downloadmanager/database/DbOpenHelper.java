d9d572dd9097d12ac5070fbfa54e9ea1b69028f7
Anton <nookieaaa@hotmail.com>
2015-10-28 20:47:41 +0200

    create tables scripts gitignore

--- app/src/main/java/com/nookdev/downloadmanager/database/DbOpenHelper.java
+++ app/src/main/java/com/nookdev/downloadmanager/database/DbOpenHelper.java
@@ -4,8 +4,9 @@ package com.nookdev.downloadmanager.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.BaseColumns;
 
-public class DbOpenHelper extends SQLiteOpenHelper {
+public class DbOpenHelper extends SQLiteOpenHelper implements BaseColumns {
 
     public static final int DB_VERSION = 1;
     public static final String DB_NAME = "downloads.db";
@@ -14,12 +15,23 @@ public class DbOpenHelper extends SQLiteOpenHelper {
     public static class TasksDB{
 
         public static final String TABLE_NAME = "tasks";
+        public static final String CREATE_DB_SCRIPT = "create table " + TABLE_NAME +
+                " ( " +
+                BaseColumns._ID + " integer primary key autoincrement, "+
+                Columns.FILENAME + " text not null, " +
+                Columns.EXTENSION + " text not null, " +
+                Columns.OUTPUT + " text not null, " +
+                Columns.SOURCE + " text not null, " +
+                Columns.INITIAL_SIZE + " integer not null, " +
+                Columns.STATUS + " integer not null, " +
+                Columns.PARTS_COUNT + " integer not null "
+                + " ) ";
+
+
         public static class Columns{
-            public static final String ID = "_ID";
             public static final String SOURCE = "source";
             public static final String OUTPUT = "output";
             public static final String INITIAL_SIZE = "size";
-            public static final String PARTS = "parts";
             public static final String STATUS = "status";
             public static final String FILENAME = "filename";
             public static final String EXTENSION = "extension";
@@ -31,10 +43,21 @@ public class DbOpenHelper extends SQLiteOpenHelper {
     //db for keeping progress for each task
     public static class ProgressDB{
         public static final String TABLE_NAME = "progress";
-        public static final String CREATE_DB_SCRIPT = "";
+        public static final String CREATE_DB_SCRIPT = "create table " + TABLE_NAME +
+                " ( " +
+                BaseColumns._ID + " integer primary key autoincrement, "+
+                Columns.OUTPUT + " text not null, " +
+                Columns.SOURCE + " text not null, " +
+                Columns.PART_SIZE + " integer not null, " +
+                Columns.START_BYTE + " integer not null, " +
+                Columns.END_BYTE + " integer not null, " +
+                Columns.STATUS + " integer not null, " +
+                Columns.TASK_ID + " integer not null, " +
+                "foreign key ("+ Columns.TASK_ID + ") references "
+                    + TasksDB.TABLE_NAME + " ( " + BaseColumns._ID + " )"
+                + " ) ";
 
         public static class Columns{
-            public static final String ID = "_ID";
             public static final String TASK_ID = "task_id";
             public static final String START_BYTE = "start_byte";
             public static final String END_BYTE = "end_byte";
@@ -53,8 +76,18 @@ public class DbOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        createTasksDb(db);
+        createProgressDb(db);
     }
+
+    private void createProgressDb(SQLiteDatabase db) {
+        db.execSQL(ProgressDB.CREATE_DB_SCRIPT);
+    }
+
+    private void createTasksDb(SQLiteDatabase db) {
+        db.execSQL(TasksDB.CREATE_DB_SCRIPT);
+    }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
